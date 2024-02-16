572f4469a54d32e78acdb352e0988c413eb62b94
Leon <leon.muriuki@student.moringaschool.com>
2024-02-16 21:00:42 +0300

    Added Leads table

--- models/models.py
+++ models/models.py
@@ -9,3 +9,14 @@ class SalesAssociate(Base):
     first_name = Column(String)
     last_name = Column(String)
     leads = relationship('Lead', back_populates='sales_associate')
+
+class Lead(Base):
+    __tablename__ = 'lead'
+    id = Column(Integer, primary_key=True)
+    customer_name = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    sales_associate_id = Column(Integer, ForeignKey('sales_associate.id'))
+    sales_associate = relationship('SalesAssociate', back_populates='leads')
+    converted_customer_id = Column(Integer, ForeignKey('converted_customer.id'))
+    converted_customer = relationship('ConvertedCustomer', back_populates='leads')
