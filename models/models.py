90a72ce6fb6d3a47e1c8ff9ee87ce1478c6870f1
Leon <leon.muriuki@student.moringaschool.com>
2024-02-16 20:59:40 +0300

    Added Sales Associate table

--- models/models.py
+++ models/models.py
@@ -2,3 +2,10 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
+
+class SalesAssociate(Base):
+    __tablename__ = 'sales_associate'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    leads = relationship('Lead', back_populates='sales_associate')
