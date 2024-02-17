7168f5b322712f45de990b97938a54b3023cda34
Leon <leon.muriuki@student.moringaschool.com>
2024-02-17 10:21:16 +0300

    Created engine

--- cli.py
+++ cli.py
@@ -1,3 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models.models import Base, SalesAssociate, Lead, ConvertedCustomer
+
+engine = create_engine('sqlite:///crm.db')
+Base.metadata.bind = engine
+Session = sessionmaker(bind=engine)
+session = Session()
